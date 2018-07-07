8ddb04cc8b3d006cf63fabbc588b9ec413f0a6e4
FsmDeveloper <smshakil1600@gmail.com>
2018-07-07 09:29:16 +0600

    fix

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const routes = require('./routes/api');
+
 const url = 'mongodb://admin:admin369147@ds223161.mlab.com:23161/db_contact'
 mongoose.connect(url).then(res => {
   console.log('Mongo connected!');
 }).catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send({
-    msg: 'Working'
-  });
-});
+app.use('/api', routes);
 
 
 
